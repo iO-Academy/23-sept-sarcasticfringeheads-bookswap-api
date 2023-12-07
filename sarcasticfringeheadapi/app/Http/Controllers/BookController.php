8941c1420b71040f9a13e02f6636565ccbe98701
shellycs50 <lebaguettemuncherer@gmail.com>
2023-12-07 17:16:34 +0000

    made getAllBooks function and added route

--- sarcasticfringeheadapi/app/Http/Controllers/BookController.php
+++ sarcasticfringeheadapi/app/Http/Controllers/BookController.php
@@ -13,8 +13,86 @@ class BookController extends Controller
         $this->book = $book;
     }
 
-    public function getAllBooks()
+    public function getAllBooks(Request $request)
     {
-        // ** TODO ** 
+        $books = $this->book->query();
+        $books = $books->get();
+        foreach($books as $book)
+        {
+            $book->genre;
+        }
+
+        if (count($books) === 0)
+        {
+            return response()->json([
+                'message' => 'No Books Found'
+            ]);
+        }
+        return response()->json(["message" => "Books successfully retrieved", "data" => $books], 200);
+        //may need to edit $books variable to delete multiple instances of genre_id if not handled by laravel automatically. (because book table has genre_id and genre table has id)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    ** dont use yet. part of later stories. ** 
+    public function getAllBooksWithFiltering(Request $request)
+    {
+        $request->validate([
+            'claimed' => 'nullable|boolean',
+            'genre' => 'nullable|integer',
+            'search' => 'nullable|string'
+        ]);
+
+        $books = $this->book->query();
+
+        if($request->has('claimed'))
+        {
+            if ($request->claimed)
+            $books->where('claimed', $request->claimed);
+        }
+        if ($request->has('search'))
+        {
+            $searchTerm = $request->search;
+            $books->where('name', 'like', '%'. $searchTerm .'%')
+            ->orWhere('author', 'like', '%'. $searchTerm . '%');
+        }
+        if ($request->has('genre'))
+        {
+            $books->where('genre', $request->genre);
+        }
+
+        $books = $books->get();
+        
+        foreach($books as $book)
+        {
+            $book->genre;
+        }
+
+        if (count($books) === 0)
+        {
+            return response()->json([
+                'message' => 'No Books Found'
+            ]);
+        }
+
+        return response()->json(["message" => "Books successfully retrieved", "data" => $books], 200);
+    }
+
+    */
